484f777de0fbfedda894fb630f1c4af80976db1e
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-17 09:18:16 +0000

    Add Card component to display title and content

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,10 @@
+import { CardProps } from "../../interfaces";
+
+export default function Card({ title, content }: CardProps) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+}
