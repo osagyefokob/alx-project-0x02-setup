edabbbb5f5b4d4a6aa66780b1baf3feef4bc05b2
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-17 09:40:07 +0000

    Create PostsPage component with header and title

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <div>
+      <Header />
+      <div style={{ padding: 16 }}>
+        <h1>This is the Posts page</h1>
+      </div>
+    </div>
+  );
+}
