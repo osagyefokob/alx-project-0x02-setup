e7be3a7ff3d0b3d3537a449415eaec0db0f8db44
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-17 09:36:49 +0000

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
+    <header style={{ padding: "12px 16px", background: "#f5f5f5" }}>
       <nav>
-        <ul>
+        <ul style={{ display: "flex", gap: "16px", listStyle: "none" }}>
           <li><Link href="/home">Home</Link></li>
           <li><Link href="/about">About</Link></li>
+          <li><Link href="/posts">Posts</Link></li>
         </ul>
       </nav>
     </header>
