cd228dc1a0238354568a1368b934e329cdb659e2
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-17 09:32:52 +0000

    Add ButtonProps interface for button customization

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -8,3 +8,9 @@ export interface Post {
   title: string;
   content: string;
 }
+
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  label: string;
+}
