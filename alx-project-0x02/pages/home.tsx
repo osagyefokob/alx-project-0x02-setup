73ed455cc19f2f4aea850f0dabcdef99d7443aa3
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-17 09:19:01 +0000

    Add Card components to HomePage
    Updated HomePage component to include two Card components.

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,3 +1,19 @@
+import Card from "../components/common/Card";
+
 export default function HomePage() {
-  return <h1>This is the Home page</h1>;
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+
+      <Card
+        title="First Card"
+        content="This is content for the first card."
+      />
+
+      <Card
+        title="Second Card"
+        content="This is content for the second card."
+      />
+    </div>
+  );
 }
